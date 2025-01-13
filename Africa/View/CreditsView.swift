53e4444cff9c0422f79c3271d823fe6a4eae21de
Mevlüt Sönmez <sonmezcan1997@gmail.com>
2025-01-13 15:08:15 +0300

    first commit

---
+++ Africa/View/CreditsView.swift
@@ -0,0 +1,24 @@
+
+import SwiftUI
+
+struct CreditsView: View {
+  var body: some View {
+    VStack {
+      Image("compass")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 128, height: 128)
+    
+    } //: VSTACK
+    .padding()
+    .opacity(0.4)
+  }
+}
+
+struct CreditsView_Previews: PreviewProvider {
+  static var previews: some View {
+    CreditsView()
+      .previewLayout(.sizeThatFits)
+      .padding()
+  }
+}
